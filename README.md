76e8d35b2209e7969d8d6363c83bfcf59daa83eb
Andrew L. Samaha <andrew.samaha@gmail.com>
2021-09-06 10:04:13 -0400

    identified foreclosed and notforeclosed parcels

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## File Index
 ### Data
+* **/Datasets/foreclosed_folios.pickle** - A list of folios that had at least one foreclosure in the time period
+* **/Datasets/notforeclosed_folios.pickle** - A list of folios that had no foreclosures in the time preiod
 * **/Datasets/residencial_conveyances_jan2000_and_up.pickle** - Conveyances and their associated parcels for residential properties (DOR_CODES < 1000)
+* **/Datasets/residencial_conveyances_jan2000_and_up_ohe.pickle** - A one-hot-encoded version of the categorical variables in the above.
 ### Models
 * **/devsrc/rea_cd_autoencoder.py** - Contains the class REA_CD_Autoencoder that uses a trained autoencoder to convert the categorical column REA_CD (Reason Code) to a two-dimensional continuous output. Only handles predictions, does not contain model training or build methods.
 * **/devsrc/SlimAutoencoder.py** - Contains the class SilmAutoencoderBuild() that contains methods to build the second-stage autoencoder, as well as prepare the data.
--- volume/devsrc/datawrangler.py
+++ volume/devsrc/datawrangler.py
@@ -1,42 +1,60 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+
 class DataWrangler():
     def __init__(self):
         #self.df_original = df.copy()
-        self.columns_linear = ['STR', 'tBEDS', 'tBATHS', 'tSTORIES', 'tUNITS', 'tBLDGS', 'ACT',
-                                'EFF', 'HEAT_AR', 'BASE', 'ACREAGE', 'REA_CD_encoded_x', 'REA_CD_encoded_y']
+        self.columns_linear = ['STR', 'tBEDS', 'tBATHS', 'tSTORIES', 'tUNITS', 'tBLDGS', 'ACT', 'S_AMT_x',
+                                 'HEAT_AR', 'BASE', 'ACREAGE',
+                                'JUST','LAND','BLDG','EXF','EFF', 'TAX_VAL','ASD_VAL']
         #self.columns_to_onehotencode = []
-        self.columns_to_onehotencode = ["HBHC_x","S_TYPE","MUNI",'TAXDIST','TIF','SITE_ZIP','VI_y','QU']
-        self.cols_to_drop = ['PIN_x','S_AMT_x','SUB_x','VI_x','GRANTOR','GRANTEE','DOC_NUM','TYPE','PIN_y','OWNER','ADDR_1',
+        self.columns_to_onehotencode = ["NBHC_x","S_TYPE","MUNI",'TAXDIST','TIF','SITE_ZIP','VI_y','QU','DOR_CODE']
+        self.cols_to_drop = ['PIN_x','SUB_x','VI_x','GRANTOR','GRANTEE','DOC_NUM','TYPE','PIN_y','OWNER','ADDR_1',
                 'ADDR_2','CITY','STATE','ZIP','COUNTRY','SUB_y','SITE_CITY','LEGAL1','LEGAL2','LEGAL3','LEGAL4','DBA','STRAP',
-                'JUST','LAND','BLDG','EXF','ASD_VAL','TAX_VAL','SD1','SD2','S_DATE_y','S_AMT_y','NBHC_y','Edit_dt','geometry',
-                'SITE_ADDR','DOR_C','OR_BK','OR_PG', "S_TYPE","MUNI",'TAXDIST','TIF','SITE_ZIP','VI_y','QU']
-        self.cols_to_drop += ['DOR_CODE']
+                'SD1','SD2','S_DATE_y','S_AMT_y','NBHC_y','Edit_dt','geometry',
+                'SITE_ADDR','DOR_C','OR_BK','OR_PG']
+
         #'FOLIO'
         #'S_DATE_x'
         self.cols_to_convert_to_num = ['STR']
 
-    def prepareSalesAndParcels(self, df):
-        df = self.df_original.copy()
-        df = df.sort_values('S_DATE_x', ascending=True)
-        #df = df[757_000:] # late january 2000
+    def one_hot_encode(self, df,cols):
+        df = df.copy()
+        original_columns = df.columns
+        sizes = dict()
+        #sizes['original_df'] = df.shape[1]
+        for col in cols:
+            start_width = df.shape[1]
+            #df = pd.get_dummies(df, columns=[col], drop_first=False)
+            df = pd.get_dummies(df, columns=[col], drop_first=False)
+            sizes[col] = df.shape[1] - start_width + 1
+        return df, sizes, original_columns
+
+    def prepare(self, df, alreadysorted=True, cleanzipcodes=True):
+        self.df_original = df = df.copy()
+        if not alreadysorted:
+            df = df.sort_values('S_DATE_x', ascending=True)
         self.df_original = df
+        
+        
+        df = self.cleanZipCodes(df, 'SITE_ZIP')
+        df = df.drop(columns=self.cols_to_drop)
         
         self.dates = df['S_DATE_x']
         
-        df = df.drop(columns=self.cols_to_drop)
-        # convert STR to an X and Y, S=section, Township, Range
-        # df['SITE_ZIP'] = df['SITE_ZIP'].str.replace('-+[0-9]*','',regex=True).str.replace('_+','',regex=True)
-        # df = df[df['SITE_ZIP'].notna()]
-        # df = df[df['SITE_ZIP'] != '']
-        # df = df[df['SITE_ZIP'] != '99999']
-        for col in self.cols_to_convert_to_num:
-            df[col] = df[col].astype('int16')
+        #for col in self.cols_to_convert_to_num:
+        #    print(f"converting to num: {col}")
+        #    df[col] = df[col].astype('int16')
 
         self.df, self.ohe_sizes, self.original_columns = self.one_hot_encode(df, self.columns_to_onehotencode)
         self.df_prepared = self.df.copy()
-        self.scaler = MinMaxScaler()
-        self.scaler.fit(self.df)
-        self.nparray = self.scaler.transform(self.df)
         return self.df_prepared
+        #self.scaler = MinMaxScaler()
+        #self.scaler.fit(self.df)
+        #self.nparray = self.scaler.transform(self.df)
+        
 
     def cleanZipCodes(self, df, columnname):
         # convert STR to an X and Y, S=section, Township, Range
